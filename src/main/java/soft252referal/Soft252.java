541e7a1f3fc424cd0662f9653111825f430e6551
rjharris01 <richard.j.harris@students.plymouth.ac.uk>
2020-08-04 15:47:01 +0100

    Created Class files

---
+++ src/main/java/soft252referal/Soft252.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package soft252referal;
+
+/**
+ *
+ * @author richa_bfe6tpy
+ */
+public class Soft252 {
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        
+    }
+}
