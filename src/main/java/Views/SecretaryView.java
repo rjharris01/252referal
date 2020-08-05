e7f94bc6fa9b924959ad242a0cb8d16f0626e187
rjharris01 <richard.j.harris@students.plymouth.ac.uk>
2020-08-05 19:06:42 +0100

    Template GUI for Secretary view

--- src/main/java/Views/SecretaryView.java
+++ src/main/java/Views/SecretaryView.java
@@ -5,17 +5,30 @@
  */
 package Views;
 
+import java.awt.Label;
+import javax.swing.JButton;
+
 /**
  *
  * @author richa_bfe6tpy
  */
 public class SecretaryView extends javax.swing.JPanel {
 
+    private Label label1;
+    private JButton SecratryApprovePatientButton;
+    private JButton SecratryCreateAppointmentButton;
+    private JButton SecratryOrderMedicineButton;
+    private JButton SecratryRemovePatientButton;
+    private JButton SecratryRemovePatientRequestButton;
+    private JButton SecratryCompletePerscriptionButton;
+    private JButton LogoutButton;
+    private JButton SecretaryApproveAppointmentButton;
+
     /**
      * Creates new form SecretaryView
      */
     public SecretaryView() {
-        initComponents();
+        initMyComponents();
     }
 
     /**
@@ -38,6 +51,92 @@ public class SecretaryView extends javax.swing.JPanel {
             .addGap(0, 300, Short.MAX_VALUE)
         );
     }// </editor-fold>//GEN-END:initComponents
+
+    private void initMyComponents() {
+       label1 = new java.awt.Label();
+       SecratryApprovePatientButton = new javax.swing.JButton();
+       SecretaryApproveAppointmentButton = new javax.swing.JButton();
+       SecratryCreateAppointmentButton = new javax.swing.JButton();
+       SecratryOrderMedicineButton = new javax.swing.JButton();
+       SecratryRemovePatientButton = new javax.swing.JButton();
+       SecratryRemovePatientRequestButton = new javax.swing.JButton();
+       SecratryCompletePerscriptionButton = new javax.swing.JButton();
+       LogoutButton = new javax.swing.JButton();
+       
+       
+       label1.setAlignment(java.awt.Label.CENTER);
+        label1.setText("Secretary Panel");
+
+        SecratryApprovePatientButton.setText("Approve Patients");
+       
+
+        SecratryCreateAppointmentButton.setText("Create Appointment");
+        
+
+        SecratryOrderMedicineButton.setText("Order Medicine");
+        
+
+        SecratryRemovePatientButton.setText("Remove Patient (Manual)");
+        
+        SecratryRemovePatientRequestButton.setText("Remove Patient (Request)");
+        
+
+        SecratryCompletePerscriptionButton.setText("Complete Perscription");
+
+        LogoutButton.setText("Logout");
+        
+
+        SecretaryApproveAppointmentButton.setText("Approve Appointments");
+        
+
+        javax.swing.GroupLayout SecretaryMainPanelLayout = new javax.swing.GroupLayout(this);
+        this.setLayout(SecretaryMainPanelLayout);
+        SecretaryMainPanelLayout.setHorizontalGroup(
+            SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(SecretaryMainPanelLayout.createSequentialGroup()
+                .addGap(116, 116, 116)
+                .addGroup(SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(SecretaryMainPanelLayout.createSequentialGroup()
+                        .addGap(83, 83, 83)
+                        .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, 322, javax.swing.GroupLayout.PREFERRED_SIZE))
+                    .addGroup(SecretaryMainPanelLayout.createSequentialGroup()
+                        .addGroup(SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(SecratryRemovePatientRequestButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(SecratryApprovePatientButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(SecratryCompletePerscriptionButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                        .addGroup(SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(SecratryRemovePatientButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(SecratryOrderMedicineButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(SecratryCreateAppointmentButton, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(SecretaryApproveAppointmentButton))
+                    .addComponent(LogoutButton))
+                .addGap(38, 38, 38))
+        );
+        SecretaryMainPanelLayout.setVerticalGroup(
+            SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(SecretaryMainPanelLayout.createSequentialGroup()
+                .addGap(21, 21, 21)
+                .addComponent(LogoutButton)
+                .addGap(47, 47, 47)
+                .addComponent(label1, javax.swing.GroupLayout.PREFERRED_SIZE, 118, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(22, 22, 22)
+                .addGroup(SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(SecratryRemovePatientRequestButton)
+                    .addComponent(SecratryRemovePatientButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(SecratryApprovePatientButton)
+                    .addComponent(SecratryOrderMedicineButton))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(SecretaryMainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(SecratryCompletePerscriptionButton)
+                    .addComponent(SecratryCreateAppointmentButton)
+                    .addComponent(SecretaryApproveAppointmentButton))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+    }
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
