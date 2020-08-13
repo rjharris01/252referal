a6cb1e678c2f9a030e08b3f8fc042ba8d8ba2496
rjharris01 <richard.j.harris@students.plymouth.ac.uk>
2020-08-13 20:15:04 +0100

    Implemented Request Class code

--- src/main/java/models/Request.java
+++ src/main/java/models/Request.java
@@ -9,6 +9,54 @@ package models;
  *
  * @author richa_bfe6tpy
  */
-public class Request {
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Richard Harris
+ */
+public abstract class Request implements Serializable {
+    
+    private String type;
+    private int requestId;
+    
+    public String getType() { return type;}
+    public void setType(String newType){type = newType;}
+    
+    public int getRequestId() { return requestId;}
+    public void setRequestId(int newRequestId){requestId = newRequestId;}
+    
+    
+    public ArrayList<Request>  getAllRequests()
+     {
+        ArrayList<Request> requests = new ArrayList<>();
+        try {
+                        FileInputStream fis = new FileInputStream("Requests.ser");
+			ObjectInputStream ois = new ObjectInputStream(fis);
+                        requests = (ArrayList) ois.readObject();
+			
+			ois.close();
+                        fis.close();
+                        
+            } catch (FileNotFoundException  e) {
+			System.out.print("No file \n");
+		} catch (IOException | ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+        return requests; 
+     }
+    
+    public int getNextRequestId()
+    {
+        ArrayList<Request> requests = getAllRequests();
+        return requests.size();
+        
+    }
+    
     
 }
