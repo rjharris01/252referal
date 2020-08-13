9aefb8f7c3978ebd12a25f36f78829687c421961
rjharris01 <richard.j.harris@students.plymouth.ac.uk>
2020-08-13 20:18:01 +0100

    Implement code for the patient user model

--- src/main/java/models/user/Patient.java
+++ src/main/java/models/user/Patient.java
@@ -5,10 +5,51 @@
  */
 package models.user;
 
+import java.time.LocalDate;
+import java.util.Scanner;
+import models.Rating;
+import models.Appointment;
 /**
  *
  * @author richa_bfe6tpy
  */
-public class Patient {
+public class Patient extends User {
     
+    private String gender;
+    private LocalDate birthday;
+    
+    public String getGender() { return gender;}
+    public void setGender(String newGender){gender = newGender;}
+    
+    public LocalDate getBirthday() { return birthday;}
+    public void setBirthday(LocalDate newBirthday){birthday = newBirthday;}
+    
+    public void newRating(Appointment appointment)
+    {
+        Scanner userInput = new Scanner(System.in);
+        String tempInput;
+        
+        
+        Rating rating = new Rating();
+        rating.setDoctor(appointment.getDoctor());
+        rating.setPatient(appointment.getPatient());
+        rating.setAppointment(appointment);
+        
+        System.out.print("Please enter a rating: ");
+        tempInput = userInput.nextLine();
+        rating.setRating(tempInput);
+        
+        rating.getDoctor().addRating(rating);
+    }
+    
+    public void requestAccountTermination()
+    {
+          
+    
+    }
+    
+    @Override
+    public String toString() {
+        return ("Name: " + this.getName() + " ID:" + this.getUserId());
+    }
 }
