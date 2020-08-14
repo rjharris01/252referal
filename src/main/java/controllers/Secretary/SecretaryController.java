d67d6c59623bb7433b9f35b6a6cbd5e59bf0ab4a
rjharris01 <richard.j.harris@students.plymouth.ac.uk>
2020-08-14 11:57:23 +0100

    Added button listener templates

--- src/main/java/controllers/Secretary/SecretaryController.java
+++ src/main/java/controllers/Secretary/SecretaryController.java
@@ -8,6 +8,8 @@ package controllers.Secretary;
 import Views.Admin.AdminView;
 import Views.Secretary.SecretaryView;
 import controllers.Admin.AdminController;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import models.user.Administrator;
 import models.user.Secretary;
 
@@ -24,5 +26,90 @@ public class SecretaryController {
         this.theView = theView;
         this.theModel = theModel;
        
+        
+        this.theView.addSecratryApprovePatientListener(new SecratryApprovePatientListener());
+        this.theView.addSecratryCreateAppointmentListener(new SecratryCreateAppointmentListener());
+        this.theView.addSecratryOrderMedicineListener(new SecratryOrderMedicineListener());
+        this.theView.addSecratryRemovePatientListener(new SecratryRemovePatientListener());
+        this.theView.addSecratryRemovePatientRequestListener(new SecratryRemovePatientRequestListener());
+        this.theView.addSecratryCompletePerscriptionListener(new SecratryCompletePerscriptionListener());
+        this.theView.addLogoutListener(new LogoutListener());
+        this.theView.addSecretaryApproveAppointmentListener(new SecretaryApproveAppointmentListener());
+        
+        
     }
+    
+    class SecratryApprovePatientListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class SecratryCreateAppointmentListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class SecratryOrderMedicineListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class SecratryRemovePatientListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class SecratryRemovePatientRequestListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class SecratryCompletePerscriptionListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class LogoutListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    class SecretaryApproveAppointmentListener implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        }
+        
+    }
+    
+    
 }
