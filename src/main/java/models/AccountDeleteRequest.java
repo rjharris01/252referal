e26942a32c820579cf00cfca401d84e248186d55
rjharris01 <richard.j.harris@students.plymouth.ac.uk>
2020-08-15 00:46:41 +0100

    Scaffolded Class

---
+++ src/main/java/models/AccountDeleteRequest.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author richa_bfe6tpy
+ */
+public class AccountDeleteRequest extends Request {
+    private String userId;
+    
+    public String getUserId() { return userId;}
+    public void setUserId(String newUserId){userId = newUserId;}
+    
+    
+}
---
+++ src/main/java/models/Medicine.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author richa_bfe6tpy
+ */
+public class Medicine implements Serializable {
+    private int medId;
+    private String name;
+    private int stock;
+    
+    
+    
+    public int getMedId(){return medId;}
+    public void setMedId(int newMedId){medId = newMedId;}
+    
+    public String getName() { return name;}
+    public void setName(String newName){name = newName;}
+    
+    public int getStock(){return stock;}
+    public void setStock(int newStock){stock = newStock;}
+    
+    @Override
+    public String toString() {
+        return (this.getName());
+    }
+    
+}
